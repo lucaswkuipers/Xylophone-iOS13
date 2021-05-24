835bd43ea7d35b8580e8a5e36e9b13d984237c4b
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-05-24 20:37:41 -0300

    :sparkles: Added sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let note = sender.titleLabel?.text
+        playSound(on: note!)
+    }
     
-
+    func playSound(on note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 }
 
